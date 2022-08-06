050d9580927a474ecc4fc9197d439bab95c6b719
Stephen Olayemi <stephen.olayemi@innotelligent.com>
2022-08-06 14:58:11 +0100

    Subsequent commit

--- src/components/layout/FooterLayout.tsx
+++ src/components/layout/FooterLayout.tsx
@@ -1,4 +1,5 @@
-import React from "react"
+import React from "react";
+import { Link } from "react-router-dom";
 
 function FooterLayout() {
     return (
@@ -13,53 +14,46 @@ function FooterLayout() {
                             <p>SUBSCRIBE TO OUR NEWSLETTER</p>
                             <form data-toggle="validator"><input type="email" className="input-newsletter"
                                 placeholder="Enter your email" name="EMAIL" required  autoComplete="off" /><button
-                                type="submit">Subscribe Now <i className="bx bx-paper-plane"></i></button>
+                                type="submit">Subscribe Now <i className="fa fa-envelope-o"></i></button>
                             <div id="validator-newsletter" className="form-result"></div>
                             </form>
                         </div>
                         <ul className="social-links">
-                            <li><a href="#" target="_blank" className="facebook"><i className="bx bxl-facebook"></i></a></li>
-                            <li><a href="#" target="_blank" className="twitter"><i className="bx bxl-twitter"></i></a></li>
-                            <li><a href="#" target="_blank" className="linkedin"><i className="bx bxl-linkedin"></i></a></li>
-                            <li><a href="#" target="_blank" className="instagram"><i className="bx bxl-instagram"></i></a></li>
+                            <li><a href="#" target="_blank" className="facebook"><i className="fa fa-facebook-square"></i></a></li>
+                            <li><a href="#" target="_blank" className="twitter"><i className="fa fa-twitter"></i></a></li>
+                            <li><a href="#" target="_blank" className="linkedin"><i className="fa fa-linkedin"></i></a></li>
+                            <li><a href="#" target="_blank" className="instagram"><i className="fa fa-instagram"></i></a></li>
                         </ul>
                         </div>
                     </div>
                     <div className="col-lg-2 col-sm-6 col-md-6">
                         <div className="single-footer-widget">
-                        <h3>Cryptocurrency</h3>
+                        <h3>eNaira</h3>
                         <ul className="services-links">
-                            <li><a href="https://novis-ts.envytheme.com/buy/">Buy Bitcoin</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/buy/">Buy BNB</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/buy/">Buy Ethereum</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/buy/">Buy Ripple</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/buy/">Buy Litecoin</a></li>
+                            <li><Link to="/">Buy eNaira</Link></li>
+                            <li><Link to="/">Deposit eNaira</Link></li>
+                            <li><Link to="/">Withdraw eNaira</Link></li>
                         </ul>
                         </div>
                     </div>
                     <div className="col-lg-3 col-sm-6 col-md-6">
                         <div className="single-footer-widget pl-5">
-                        <h3>Resources</h3>
-                        <ul className="quick-links">
-                            <li><a href="https://novis-ts.envytheme.com/trade/">Trade</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/guides/">Guides</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/wallet/">Wallets</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/faq/">FAQ</a></li>
-                            <li><a href="https://novis-ts.envytheme.com/contact/">Contact</a></li>
-                        </ul>
+                            <h3>Resources</h3>
+                            <ul className="quick-links">
+                                <li><Link to="https://novis-ts.envytheme.com/trade/">Trade</Link></li>
+                                <li><Link to="https://novis-ts.envytheme.com/guides/">Guides</Link></li>
+                                <li><Link to="https://novis-ts.envytheme.com/wallet/">Wallets</Link></li>
+                            </ul>
                         </div>
                     </div>
                     <div className="col-lg-3 col-sm-6 col-md-6">
                         <div className="single-footer-widget">
                         <h3>Contact Info</h3>
                         <ul className="footer-contact-info">
-                            <li>Address: 2750, Quadra Street Victoria, Canada</li>
-                            <li>Email: <a
-                                href="https://novis-ts.envytheme.com/cdn-cgi/l/email-protection#98f0fdf4f4f7d8d6f7eef1ebb6fbf7f5"><span
-                                className="__cf_email__"
-                                data-cfemail="ddb5b8b1b1b29db3b2abb4aef3beb2b0">[email&#160;protected]</span></a></li>
-                            <li>Phone: <a href="tel:+44587154756">+1416-555-0896</a></li>
-                            <li>Fax: <a href="tel:+44587154756">+1416-555-0477</a></li>
+                            <li>Address: AFF, CBN Nigeria Hackerthon</li>
+                            <li>Email: <Link to="/">eNairaNigeria.cbn@aff.com</Link></li>
+                            <li>Phone: <Link to="tel:++234eNairaNigeria">+234eNairaNigeria</Link></li>
+                            <li>Fax: <Link to="tel:+234eNairaNigeria">+234eNairaNigeria</Link></li>
                         </ul>
                         </div>
                     </div>
